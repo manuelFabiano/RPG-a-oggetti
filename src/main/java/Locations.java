a703c24ed1c6ebef89fa6fd1b33f66e1a3e546b9
Sergio333333 <sergiobucca@gmail.com>
2023-05-21 21:38:36 +0200

    !

---
+++ src/main/java/Locations.java
@@ -0,0 +1,35 @@
+import java.until.Random;
+
+public class Locations {
+    private String nome;
+    private String descrizione;
+
+    // Costruttore
+    public Locations(String nome, String descrizione) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+    }
+
+    // Metodi getter
+    public String getNomelocation() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+    List<Locations> listaLocations = new ArrayList<>();
+listaLocations.add(new Locations("Casa", "Ti trovi nella tua accogliente casa."));
+listaLocations.add(new Locations("Foresta", "Ti trovi in una fitta foresta."));
+listaLocations.add(new Locations("Caverna", "Ti trovi in una buia caverna."));
+listaLocations.add(new Locations("Lago", "Ti trovi in un lago ricco di pesci."));
+// Aggiungi altre location alla lista
+
+    Random random = new Random();
+    int indiceCasuale = random.nextInt(listaLocations.size());
+    Locations locationCasuale = listaLocations.get(indiceCasuale);
+
+System.out.println("Sei a " + locationCasuale.getNomelocation());
+System.out.println(locationCasuale.getDescrizione());
+
+}
